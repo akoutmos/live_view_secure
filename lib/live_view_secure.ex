5c2c0db7f93c0bdf9484c10412b9efc3c7574de5
Alex Koutmos <akoutmos@gmail.com>
2021-10-13 20:52:58 -0400

    Initial commit

---
+++ lib/live_view_secure.ex
@@ -0,0 +1,18 @@
+defmodule LiveViewSecure do
+  @moduledoc """
+  Documentation for `LiveViewSecure`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LiveViewSecure.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/live_view_secure_test.exs
@@ -0,0 +1,8 @@
+defmodule LiveViewSecureTest do
+  use ExUnit.Case
+  doctest LiveViewSecure
+
+  test "greets the world" do
+    assert LiveViewSecure.hello() == :world
+  end
+end
